bbf5455be7953aeb28ebac37c4299edba078a0ac
kadhirash <kadhirash@gmail.com>
2020-05-20 07:39:28 -0700

    Finished parts 2 and 3

--- Lab10_concurrentSynchSMs/turnin/ksiva001_lab10_part2.c
+++ Lab10_concurrentSynchSMs/turnin/ksiva001_lab10_part2.c
@@ -1,9 +1,9 @@
 /*	Author: lab
  *  Partner(s) Name: Kadhirash Sivakumar
  *	Lab Section: 021
- *	Assignment: Lab # 10  Exercise # 3
+ *	Assignment: Lab # 10  Exercise # 2
  *	Exercise Description: [optional - include for your own benefit]
- *	Blinking and 3 LED's, but periods are different now and speaker
+ *	Blinking and 3 LED's, but periods are different now
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -66,41 +66,6 @@ void BlinkingLEDSM_Tick(){
 	}
 }
 
-enum SpeakerSM{SpeakerStart, SpeakerInit, SpeakerOn, SpeakerOff} SpeakerState;
-
-unsigned char speaker = 0x00;
-#define A2 (~PINA & 0x04)
-
-void SpeakerSM_Tick(){
-	switch(SpeakerState){
-		case SpeakerStart:
-			SpeakerState = SpeakerInit; break;
-		case SpeakerInit:
-			SpeakerState = SpeakerOn; break;
-		case SpeakerOff:
-			if(A2 == 0x04){
-				SpeakerState = SpeakerOn;
-			}
-			break;
-		case SpeakerOn:
-			if(A2 != 0x00){
-				SpeakerState = SpeakerOff;
-			}
-			break;
-		default:
-			break;
-	}
-	switch(SpeakerState){
-		case SpeakerInit:
-		case SpeakerStart:
-		case SpeakerOff:
-			speaker = 0x00; break;
-		case SpeakerOn:
-			speaker = 0x01; break;
-		default:
-			break;
-	}
-}
 enum CombineLEDsSM{CombineLEDStart} CombineLEDState;
 
 void CombineLEDsSM_Tick(){
@@ -112,7 +77,7 @@ void CombineLEDsSM_Tick(){
 	}
 	switch(CombineLEDState){
 		case CombineLEDStart:
-			PORTB = threeLEDs | (blinkingLED << 3) | (speaker << 4);
+			PORTB = threeLEDs | (blinkingLED << 3);
 			break;
 		default:
 			break;
@@ -121,18 +86,17 @@ void CombineLEDsSM_Tick(){
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRB = 0xFF;	PORTB = 0x00;
-	DDRA = 0x00;	PORTA = 0xFF;
+
 	unsigned short ThreeLEDs_Time = 0;
 	unsigned short BlinkingLED_Time = 0;
-	unsigned short Speaker_Time = 0;
-	const unsigned short period = 2; 
+	const unsigned short period = 100; // common gcd between 300, 1000
 
 
 	ThreeLEDsState = ThreeLEDsStart;
 	BlinkingLEDState = BlinkingLEDStart;
 	CombineLEDState = CombineLEDStart;
 
-	TimerSet(2);
+	TimerSet(100);
 	TimerOn();
     /* Insert your solution below */
     while (1) {
@@ -144,19 +108,12 @@ int main(void) {
 		BlinkingLEDSM_Tick();
 		BlinkingLED_Time = 0;
 	}
-	if(Speaker_Time >= 2){
-		SpeakerSM_Tick();
-		Speaker_Time = 0;
-	}
-
-
 	CombineLEDsSM_Tick();
 	
 	while(!TimerFlag);
 	TimerFlag = 0;
 	ThreeLEDs_Time += period;
 	BlinkingLED_Time += period;
-	Speaker_Time += period;
     }
     return 1;
 }
